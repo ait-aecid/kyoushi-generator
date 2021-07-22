bf30aec5c98f23c3f209a94a0ba0ab9c6c1c27ee
Maximilian Frank <maximilian.frank@ait.ac.at>
2021-07-22 11:29:58 +0200

    Add basic docs for plugin module

--- src/cr_kyoushi/generator/plugin.py
+++ src/cr_kyoushi/generator/plugin.py
@@ -1,3 +1,8 @@
+"""
+The plugin module contains the interface definitions and utility functions used
+to load generator plugins used in the TIM context templating configuration.
+"""
+
 import sys
 
 from typing import (
@@ -35,20 +40,48 @@ else:
 
 @runtime_checkable
 class Generator(Protocol):
+    """Interface definition of generator plugin.
+
+    Generator plugins can be added through the entry points system.
+    A generator must have a name and a create method accepting a seed store.
+    The create method must create some sort of random data generator object initialized
+    with a seed/s from the given seed store.
+    This object will then be available in TIM context templates Jinja2 logic.
+    """
+
     def __init__(self):
+        """Generators must have an init method requirering no arguments."""
         ...
 
     @property
     def name(self) -> str:
+        """The name under which the random data generator object will be accessible."""
         ...
 
     def create(self, seed_store: SeedStore) -> Any:
+        """The main function used to create random data generator objects.
+
+        Args:
+            seed_store: The seed store to be used for random seed generation.
+
+        Returns:
+            A random data generator object or function.
+        """
         ...
 
 
 def _check_plugin_allowed(
     ep: EntryPoint, plugin_config: PluginConfig
 ) -> Optional[Type[Generator]]:
+    """Utility function used to check if the given entry point will be loaded.
+
+    Args:
+        ep: The entry point plugin
+        plugin_config: The plugin configuration
+
+    Returns:
+        The loaded entry point or `None` if the configration policy disallows loading it.
+    """
     if (
         # entry point name must be in an include pattern
         any([pattern.match(ep.name) for pattern in plugin_config.include_names])
@@ -66,6 +99,14 @@ def _check_plugin_allowed(
 
 
 def get_generators(plugin_config: PluginConfig) -> List[Generator]:
+    """Utility function for getting a list of all available generator plugins.
+
+    Args:
+        plugin_config: The plugin configuration policy
+
+    Returns:
+        List of the loaded generator plugins.
+    """
     return [
         ep.load()()
         for ep in entry_points(group=GENERATOR_ENTRYPOINT)
